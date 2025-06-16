6c261b5a0058998e337711713cce04d82983ad53
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-16 10:38:01 +0300

    Update fxmanifest.lua

--- tpz_clothing/fxmanifest.lua
+++ tpz_clothing/fxmanifest.lua
@@ -3,15 +3,11 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Nosmakos'
-description 'TPZ-CORE - Clothing'
+description 'TPZ-CORE - Clothing Stores & Wardrobes'
 version '1.0.0'
 
 shared_scripts { 'config.lua', 'locales.lua' }
-
 server_scripts { 'server/*.lua' }
-
 client_scripts { 'client/*.lua' }
-
-exports { "openWardrobe" }
 
 lua54 'yes'
