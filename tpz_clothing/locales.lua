10af0a65783d3c450f7717b08492fa6c27f20df0
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-16 12:02:43 +0300

    Update locales.lua

--- tpz_clothing/locales.lua
+++ tpz_clothing/locales.lua
@@ -7,25 +7,15 @@ Locales = {
     ['CLOTHING_TITLE']         = "Clothing Store",
     ['MAKEUP_TITLE']           = "Makeup Store",
 
-    ['CUSTOMIZE_CLOTHING_DESCRIPTION']   = "Customize new character outfits",
-    ['CUSTOMIZE_MAKEUP_DESCRIPTION']     = "Customize new makeup outfits",
-
+    ['CUSTOMIZE_CLOTHING_DESCRIPTION']   = "Customize new character outfits", 
     ['NO_OUTFITS_AVAILABLE']    = "~e~You don't have any outfits available.",
-    ['NO_MAKEUP_OUTFITS_AVAILABLE'] = "~e~You don't have any makeup outfits available.",
-
     ['TOTAL_TYPES']             = "Total Types: ",
 
     ['SAVE_OUTFIT_TITLE']       = "Save Current Outfit",
     ['SAVE_OUTFIT_DESCRIPTION'] = "What name would you like to set for this outfit?",
 
-    ['SAVE_MAKEUP_TITLE']       = "Save Current Makeup",
-    ['SAVE_MAKEUP_DESCRIPTION'] = "What name would you like to set for this makeup?",
-
     ['RENAME_OUTFIT_TITLE']       = "Rename Selected Outfit",
     ['RENAME_OUTFIT_DESCRIPTION'] = "What name would you like to set for this outfit?",
-
-    ['RENAME_MAKEUP_TITLE']       = "Rename Selected Makeup",
-    ['RENAME_MAKEUP_DESCRIPTION'] = "What name would you like to set for this makeup?",
 
     ['ACCEPT_BUTTON']           = "ACCEPT",
     ['DECLINE_BUTTON']          = "DECLINE",
@@ -34,11 +24,6 @@ Locales = {
     ['REPLACED_OUTFIT']         = "~t6~You have successfully replaced your default outfit.",
     ['RENAMED_OUTFIT']          = "~t6~You have successfully renamed an outfit",
     ['DELETED_OUTFIT']          = "~t6~You have successfully removed an outfit",
-
-    ['SAVED_MAKEUP']            = "~t6~You have successfully saved a makeup.",
-    ['REPLACED_MAKEUP']         = "~t6~You have successfully replaced your default makeup.",
-    ['RENAMED_MAKEUP']          = "~t6~You have successfully renamed a makeup.",
-    ['DELETED_MAKEUP']          = "~t6~You have successfully removed a makeup.",
 
     ['WARDROBE_TITLE']          = "Wardrobe",
 
@@ -49,21 +34,10 @@ Locales = {
             desc = "Customize your character clothing based on your personal preferences.",
         },
 
-        ['MAKEUP'] = {
-            label = "Makeup",
-            desc = "Customize your character makeup based on your personal preferences.",
-        },
-
         ['SAVE'] = {
             label = "Save Outfit",
             desc = "Save your current character clothing outfit.",
         },
-
-        ['SAVE_MAKEUP'] = {
-            label = "Save Makeup Outfit",
-            desc = "Save your current character makeup outfit.",
-        },
-
 
         ['EXIT'] = {
             label = "Exit",
@@ -78,19 +52,9 @@ Locales = {
             label = "Outfits",
             desc = "",
         },
-        
-        ['MAKEUP'] = {
-            label = "Makeup",
-            desc = "",
-        },
 
         ['APPLY'] = {
             label = "Apply Outfit",
-            desc = "",
-        },
-
-        ['APPLY_MAKEUP'] = {
-            label = "Apply Makeup",
             desc = "",
         },
 
@@ -99,28 +63,13 @@ Locales = {
             desc = "",
         },
 
-        ['SET_DEFAULT_MAKEUP'] = {
-            label = "Set Default Makeup",
-            desc = "",
-        },
-
         ['RENAME'] = {
             label = "Rename Outfit",
             desc = "",
         },
         
-        ['RENAME_MAKEUP'] = {
-            label = "Rename Makeup",
-            desc = "",
-        },
-
         ['DELETE'] = {
             label = "Delete Outfit",
-            desc = "",
-        },
-
-        ['DELETE_MAKEUP'] = {
-            label = "Delete Makeup",
             desc = "",
         },
 
@@ -135,15 +84,5 @@ Locales = {
         },
 
     },
-
-    ['MAKEUP'] = {
-
-        ['TEXTURES']         = "Textures",
-        ['COLORS']           = "Colors",
-        ['SECONDARY_COLORS'] = "Secondary Colors",
-        ['VARIANTS']         = "Variants",
-        ['OPACITY']          = "Opacity",
-    },
-
 
 }
