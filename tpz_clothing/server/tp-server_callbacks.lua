19177ba8e928e8963769bfb0ada1406c5218c4c2
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-31 11:36:44 +0300

    Update tp-server_callbacks.lua

--- tpz_clothing/server/tp-server_callbacks.lua
+++ tpz_clothing/server/tp-server_callbacks.lua
@@ -4,42 +4,21 @@ local TPZ = exports.tpz_core:getCoreAPI()
 --[[ Callbacks  ]]--
 -----------------------------------------------------------
 
-TPZ.addNewCallBack("tpz_clothing:callbacks:getPlayerDefaultOutfit", function(source, cb)
-	local _source         = source
-
-	local xPlayer         = TPZ.GetPlayer(_source)
-	local charidentifier  = xPlayer.getCharacterIdentifier()
-
-    exports["ghmattimysql"]:execute("SELECT * FROM `characters` WHERE `charidentifier` = @charidentifier", { ['charidentifier'] = charidentifier }, function(result)
-		
-		return cb(result[1].skinComp)
-	
-	end)
-end)
-
-TPZ.addNewCallBack("tpz_clothing:callbacks:getPlayerOutfits", function(source, cb)
+exports.tpz_core:getCoreAPI().addNewCallBack("tpz_clothing:callbacks:getPlayerOutfits", function(source, cb)
 	local _source         = source
 
 	local xPlayer         = TPZ.GetPlayer(_source)
 	local charIdentifier  = xPlayer.getCharacterIdentifier()
 
-	local outfits         = {}
+	exports["ghmattimysql"]:execute("SELECT * FROM `outfits` WHERE `charidentifier` = @charidentifier", { ["@charidentifier"] = charIdentifier }, function(result)
 
-	exports["ghmattimysql"]:execute("SELECT * FROM `outfits`", {}, function(result)
+		local returned_data = {}
 
-		if TPZ.GetTableLength(result) <= 0 then
-			return cb (nil)
+		if result and result[1] then 
+			returned_data = json.decode(result[1].outfits
 		end
 
-		for _, res in pairs (result) do
-
-			if res.charidentifier == charIdentifier then
-				table.insert(outfits, res)
-			end
-
-		end
-
-		return cb(outfits)
+		return cb(returned_data)
 	
 	end)
 
