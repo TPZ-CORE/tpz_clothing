1a363ef2a356a1b1dd282cebadd7217623ecfb1f
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-31 12:05:26 +0300

    Create tp-client_exports.lua

---
+++ tpz_clothing/client/tp-client_exports.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------
+--[[ Exports ]]--
+-----------------------------------------------------------
+
+-- @function : openWardrobe menu function.  
+exports('openWardrobe', function() OpenWardrobe() end)
+
+-- @return isBusy : returns a boolean (if player has open buying menu or wardrobes menu).
+exports('isBusy', function() return GetPlayerData().IsBusy or GetPlayerData().HasStoreOpen end)
