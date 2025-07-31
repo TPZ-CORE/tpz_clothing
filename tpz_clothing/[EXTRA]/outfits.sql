833c5fbe46080ab143e14a6320432fd1ea3cc784
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-31 13:36:05 +0300

    Update outfits.sql

--- tpz_clothing/[EXTRA]/outfits.sql
+++ tpz_clothing/[EXTRA]/outfits.sql
@@ -1,9 +1,8 @@
 
 CREATE TABLE IF NOT EXISTS `outfits` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(45) NOT NULL,
   `charidentifier` int(11) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `comps` longtext DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+  `purchased` longtext NOT NULL DEFAULT '[]',
+	`outfits` longtext NOT NULL DEFAULT '[]',
+  PRIMARY KEY (`charidentifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
