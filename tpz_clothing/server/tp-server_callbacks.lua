7ca7cf14ae9bcc62516e99fc26547672d42ce3f4
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-31 11:42:17 +0300

    Update tp-server_callbacks.lua

--- tpz_clothing/server/tp-server_callbacks.lua
+++ tpz_clothing/server/tp-server_callbacks.lua
@@ -5,21 +5,15 @@ local TPZ = exports.tpz_core:getCoreAPI()
 -----------------------------------------------------------
 
 exports.tpz_core:getCoreAPI().addNewCallBack("tpz_clothing:callbacks:getPlayerOutfits", function(source, cb)
-	local _source         = source
-
-	local xPlayer         = TPZ.GetPlayer(_source)
-	local charIdentifier  = xPlayer.getCharacterIdentifier()
-
-	exports["ghmattimysql"]:execute("SELECT * FROM `outfits` WHERE `charidentifier` = @charidentifier", { ["@charidentifier"] = charIdentifier }, function(result)
-
-		local returned_data = {}
-
-		if result and result[1] then 
-			returned_data = json.decode(result[1].outfits
-		end
-
-		return cb(returned_data)
+	local _source       = source
+	local xPlayer       = TPZ.GetPlayer(_source)
 	
-	end)
+    local ClothingData  = GetClothingData()
+	local returned_data = { outfits = {}, purchased = {} }
 
+	if ClothingData[_source] then 
+		returned_data = { outfits = ClothingData[_source].outfits, purchased = ClothingData[_source].purchased } 
+	end
+
+	return cb(returned_data)
 end)
