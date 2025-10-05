9778dc3d7fdd07ee2990e491ae08ea8579483ac4
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-10-05 23:06:55 +0300

    Delete tpz_clothing directory

--- tpz_clothing/[EXTRA]/outfits.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `outfits` (
-	`identifier` varchar(45) NOT NULL,
-	`charidentifier` int(11) DEFAULT NULL,
-	`purchased` longtext NOT NULL DEFAULT '[]',
-	`outfits` longtext NOT NULL DEFAULT '[]',
-  PRIMARY KEY (`charidentifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
--- tpz_clothing/client/tp-client_exports.lua
+++
@@ -1,9 +0,0 @@
------------------------------------------------------------
---[[ Exports ]]--
------------------------------------------------------------
-
--- @function : openWardrobe menu function.  
-exports('openWardrobe', function() OpenWardrobe() end)
-
--- @return isBusy : returns a boolean (if player has open buying menu or wardrobes menu).
-exports('isBusy', function() return GetPlayerData().IsBusy end)
